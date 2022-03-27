c0dd9d07ea51ec76dd58f73e63b4113b08ec9f9b
cooldude-222 <72757962+cooldude-222@users.noreply.github.com>
2022-03-26 17:36:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementsByTagName('h1')[0].onmouseout=
 function(){
 this.innerText='Mouse is Elsewhere';this.style.color='Red'}
+document.getElementsByNameTag('h1')[0].onmouseover=
+function (){
+this.innerText= 'Mouse Is Over';this.style.color= 'Yellow'}
